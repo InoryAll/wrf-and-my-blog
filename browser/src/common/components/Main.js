cd33207a0fecc85c6106de669ab9aa65dbeee120
Jie ren Tian <110873204@qq.com>
2017-09-01 09:42:13 +0800

    Banner.

--- browser/src/common/components/Main.js
+++ browser/src/common/components/Main.js
@@ -2,8 +2,9 @@
  *主页面组件 Main
  */
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon, Card } from 'antd';
 import '../css/main.css';
+import Banner from './Banner';
 
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
@@ -58,6 +59,9 @@ export default class Main extends React.Component {
                     </Menu>
                 </Sider>
                 <Layout>
+                    <Card className="banner-card" bordered={false}>
+                        <Banner/>
+                    </Card>
                     <Content style={{ margin: '0 16px' }}>
                         <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                             Bill is a cat.
