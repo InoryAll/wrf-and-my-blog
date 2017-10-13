d8431554105554f78dfaaaee4b4220e563627214
Jie Ren Tian <110873204@qq.com>
2017-10-13 15:09:53 +0800

    commentModal add.

--- browser/src/admin/commentManage/CommentModal.js
+++ browser/src/admin/commentManage/CommentModal.js
@@ -2,11 +2,44 @@
  * 查看/修改文章组件 commentmodal
  */
 import React from 'react';
+import { Modal, Button } from 'antd';
 
 class CommentModal extends React.Component{
+  state = {
+    loading: false,
+    visible: false,
+  };
+  handleOk = () => {
+    this.setState({ loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  };
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
   render(){
     return (
-      <div>1111</div>
+      <div>
+        <Modal
+        visible={visible}
+        title="查看/修改文章信息"
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+        footer={[
+          <Button key="back" size="large" onClick={this.handleCancel}>Return</Button>,
+          <Button key="submit" type="primary" size="large" loading={loading} onClick={this.handleOk}>
+            Submit
+          </Button>,
+        ]}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
     );
   }
 }
