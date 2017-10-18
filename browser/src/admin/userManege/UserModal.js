0572ffdc42149503c242891f2f52640ce8487067
Jie Ren Tian <110873204@qq.com>
2017-10-18 16:44:14 +0800

    UserModal init.

---
+++ browser/src/admin/userManege/UserModal.js
@@ -0,0 +1,14 @@
+/**
+ * 查看/修改用户组件
+ */
+import React from 'react';
+
+class UserModal extends React.Component {
+  render() {
+    return (
+      <div>hello world</div>
+    );
+  }
+}
+
+export default UserModal;
