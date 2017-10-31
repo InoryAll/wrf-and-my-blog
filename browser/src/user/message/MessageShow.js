826666005b1be93b13cf6332141eb0d665d03757
Jie Ren Tian <110873204@qq.com>
2017-10-31 12:08:22 +0800

    boardShow style updated.

--- browser/src/user/message/MessageShow.js
+++ browser/src/user/message/MessageShow.js
@@ -2,7 +2,7 @@
  * 留言展示组件 messageshow
  */
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Avatar, Row, Col } from 'antd';
 import './css/messageShow.css';
 
 class MessageShow extends React.Component {
@@ -12,12 +12,26 @@ class MessageShow extends React.Component {
         <h1 className="title">留言</h1>
         <hr className="message-show-split" />
         <Card className="message-card">
-          <p className="content">这里是留言内容</p>
+          <Row>
+            <Col span={24}>
+              <div className="icon-div">
+                <Avatar shape="square" size="large" icon="user" className="user-icon" />
+              </div>
+              <p className="content">这里是留言内容</p>
+            </Col>
+          </Row>
           <span className="date">日期：2017/10/31</span>
           <span className="author">作者：trj</span>
         </Card>
         <Card className="message-card">
-          <p className="content">这里是留言内容</p>
+          <Row>
+            <Col span={24}>
+              <div className="icon-div">
+                <Avatar shape="square" size="large" icon="user" className="user-icon" />
+              </div>
+              <p className="content">这里是留言内容</p>
+            </Col>
+          </Row>
           <span className="date">日期：2017/10/31</span>
           <span className="author">作者：trj</span>
         </Card>
