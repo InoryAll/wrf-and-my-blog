f29acbfad47403e5b106f04f9d142138e58e40e5
Jie Ren Tian <110873204@qq.com>
2017-11-02 14:31:58 +0800

    calender finished.

--- browser/src/user/calendar/MyCalendar.js
+++ browser/src/user/calendar/MyCalendar.js
@@ -9,20 +9,27 @@ import 'moment/locale/zh-cn';
 import './css/calender.css';
 moment.locale('zh-cn');
 
+function formatTime(timeSpan) {
+  const day = Math.floor(timeSpan / (24*3600*1000));
+  const hour = Math.floor((timeSpan % (24*3600*1000)) / (3600 * 1000));
+  const minute = Math.floor((timeSpan % (3600 * 1000)) / (60 * 1000));
+  const second = Math.floor((timeSpan % (60 * 1000)) / 1000);
+  return `${day}天${hour}小时${minute}分${second}秒`;
+}
 function onPanelChange(value, mode) {
   console.log(value, mode);
 }
 function countDown() {
     const currentTime = moment();
-    const targetTime = moment('2017/12/31');
-    const timeSpan = targetTime.diff(currentTime);
-    return timeSpan;
+    const targetTime = moment('2017-12-31');
+    const timeSpan =  targetTime.diff(currentTime);
+    return formatTime(timeSpan);
 }
 class MyCalendar extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      timeSpan: 0,
+      timeSpan: formatTime(moment('2017-12-31').diff(moment())),
     };
   }
   componentDidMount() {
@@ -44,7 +51,7 @@ class MyCalendar extends React.Component{
           <hr className="calender-split" />
           <Row>
             <Col span={8} offset={8}>
-              <div style={{ width: 290, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+              <div className="calender-box">
                 <Calendar
                   fullscreen={false}
                   onPanelChange={onPanelChange}
@@ -53,9 +60,11 @@ class MyCalendar extends React.Component{
             </Col>
           </Row>
           <Row>
-            <Col>
-              <span>倒计时:</span>
-              <span>{ this.state.timeSpan }</span>
+            <Col span={8} offset={8}>
+              <div className="countDown-container">
+                <span className="countDown-title">倒计时:</span>
+                <span className="countDown-content">{ this.state.timeSpan }</span>
+              </div>
             </Col>
           </Row>
         </Card>
