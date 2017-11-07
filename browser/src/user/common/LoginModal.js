f34ff22a9bb95f3517ef369f8c3b5694e973b702
Jie Ren Tian <110873204@qq.com>
2017-11-07 16:19:17 +0800

    icon add.

--- browser/src/user/common/LoginModal.js
+++ browser/src/user/common/LoginModal.js
@@ -38,12 +38,18 @@ class LoginModal extends React.Component{
           <Row>
             <Col span={10}>
               <Card className="QQ-login login-card">
-              
+                <span>
+                  <i className="iconfont icon-qq login-icon" />
+                </span>
+                <h1 className="login-h">QQ登录</h1>
               </Card>
             </Col>
             <Col span={10} offset={4}>
               <Card className="wechat-login login-card">
-              
+                <span>
+                  <i className="iconfont icon-weixin login-icon" />
+                </span>
+                <h1 className="login-h">微信登录</h1>
               </Card>
             </Col>
           </Row>
