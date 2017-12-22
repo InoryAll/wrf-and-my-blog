0ab8df09fa355b6a94e64396508e5cafc03e19b7
Jie Ren Tian <110873204@qq.com>
2017-12-22 08:51:24 +0800

    ..

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,7 @@ var  ReviewService=require( '../service/ReviewService');
 var  UserService=require('../service/UserService');
 var  BoardService=require('../service/BoardService');
 
+/*文章操作*/
 router.post('/comment/getSiders',CommentService.getSliders);
 router.post('/comment/getAllComment',CommentService.getAllComments);
 router.post('/comment/getCommentDetail',CommentService.getCommentDetail);
@@ -16,9 +17,11 @@ router.post('/comment/postBlog',CommentService.addComment);
 router.post('/comment/updateComment',CommentService.updateComment);
 router.post('/comment/deleteComment',CommentService.deleteComment);
 
+/*评论操作*/
 router.post('/review/addReview',ReviewService.addReview);
 router.post('/review/getReviewsById',ReviewService.getReviewsById);
 
+/*用户操作*/
 router.post('/user/login',UserService.findUser);
 router.post('/user/getAllUsers',UserService.findAllUser);
 router.post('/user/getUserDetail',UserService.findUserByName);
@@ -26,6 +29,7 @@ router.post('/user/updateUser',UserService.updateUser);
 router.post('/user/deleteUser',UserService.deleteUser);
 router.post('/user/addUser',UserService.addUser);
 
+/*留言板操作*/
 router.post('/board/addBoard',BoardService.addBoard);
 router.post('/board/getAllBoards',BoardService.getAllBoards);
 
